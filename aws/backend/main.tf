ee839521252b9624b10c4d1a7441abd961955aa9
reflection <reflection>
2024-04-16 23:47:44 +0300

    Add CI/CD Backend9

--- aws/backend/main.tf
+++ aws/backend/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
     tags = {
       "TerminationDate" = "Permanent",
       "Environment"     = "Development",
-      "Team"            = "DevOpss",
+      "Team"            = "DevOps",
       "DeployedBy"      = "Terraform",
       "Application"     = "Terraform Backend",
       "OwnerEmail"      = "artembrigaz@example.com"
